ae0834e431098e8ef3ed8119066199cb1dce4b55
Lewis Campbell <info@lewiscampbell.tech>
2022-05-29 13:07:47 +1200

    working on new type composition function

--- type.zig
+++ type.zig
@@ -31,9 +31,12 @@ pub const Sig = struct {
 
     pub fn compose(self: @This(), other: Sig) Sig {
         const max_depth_read = @maximum(self.in, other.in - self.out);
-        const final_stack_pos = -self.in + self.out - other.in + other.out;
+        const final_stack_pos = self.out - self.in + other.out - other.in;
 
-        return .{ .in = max_depth_read, .out = max_depth_read + final_stack_pos };
+        return .{
+            .in = max_depth_read,
+            .out = max_depth_read + final_stack_pos,
+        };
     }
 };
 
@@ -55,8 +58,34 @@ const NewSig = struct {
     pub fn init(in: []const Elem, out: []const Elem) NewSig {
         return NewSig{ .in = in, .out = out };
     }
+
+    pub fn compose(
+        self: @This(),
+        _: std.mem.Allocator,
+        other: NewSig,
+    ) NewSig {
+        const max_depth_read =
+            @maximum(self.in.len, other.in.len - self.out.len);
+
+        const final_stack_pos =
+            self.out.len - self.in.len + other.out.len - other.in.len;
+
+        // ([] -> [num]) * ([] -> [num]) = ([] -> [num, num])
+
+        return .{
+            .in = self.in[self.in.len - max_depth_read .. self.in.len],
+            .out = other.out[max_depth_read + final_stack_pos ..],
+        };
+    }
 };
 
 test "initialize new sig" {
     _ = NewSig.init(&.{.num}, &.{.num});
+}
+
+test "compose two operands" {
+    const left = NewSig.init(&.{}, &.{.num});
+    const right = NewSig.init(&.{}, &.{.num});
+
+    _ = left.compose(std.testing.allocator, right);
 }
