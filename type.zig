77068a30d5b8fffeaac183a7caaba406b69f4c5e
Lewis Campbell <info@lewiscampbell.tech>
2022-05-29 12:39:11 +1200

    add type.zig

---
+++ type.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+pub const Sig = struct {
+    in: i8,
+    out: i8,
+
+    pub fn init(in: i8, out: i8) Sig {
+        return Sig{ .in = in, .out = out };
+    }
+
+    pub fn format(
+        self: @This(),
+        comptime _: []const u8,
+        _: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        // TODO: hardcoding num
+
+        var in_index: usize = 0;
+        while (in_index < self.in) : (in_index += 1) {
+            try writer.writeAll("num ");
+        }
+
+        try writer.writeAll("->");
+
+        var out_index: usize = 0;
+        while (out_index < self.out) : (out_index += 1) {
+            try writer.writeAll(" num");
+        }
+    }
+
+    pub fn compose(self: @This(), other: Sig) Sig {
+        const max_depth_read = @maximum(self.in, other.in - self.out);
+        const final_stack_pos = -self.in + self.out - other.in + other.out;
+
+        return .{ .in = max_depth_read, .out = max_depth_read + final_stack_pos };
+    }
+};
+
+// pub const util = struct {
+//     identity: Sig,
+//     operand: Sig
+
+//     fn init(allocator: std.mem.Allocator) {
+
+//     }
+// };
+
+const Elem = enum { num };
+
+const NewSig = struct { in: []Elem, out: []Elem };
