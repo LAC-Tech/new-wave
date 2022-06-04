0c5a362881613aae75592b4cba2fd5690594badd
Lewis Campbell <info@lewiscampbell.tech>
2022-06-05 10:26:27 +1200

    generic type inference compiles. doubt it works

--- type.ml
+++ type.ml
@@ -3,10 +3,28 @@ open Base
 type type_sig = Op of (elem list * elem list) | Apply
 and elem = Poly of char | Mono of string | Quote of type_sig
 
-let elem_match l r = match (l, r) with
-	| (Poly _, _) -> true
-	| (_, Poly _) -> true
-	| (a, b) -> true
+(* literal equals, used for looking up in table *)
+let rec type_sig_equal ts1 ts2 = match (ts1, ts2) with
+	| (Op (is1, os1), Op (is2, os2)) -> 
+		(List.equal elem_equal is1 is2) && (List.equal elem_equal os1 os2)
+	| (Apply, Apply) -> true
+	| _ -> false
+and elem_equal e1 e2 = match (e1, e2) with 
+	| (Poly c1, Poly c2) -> Char.equal c1 c2
+	| (Mono s1, Mono s2) -> String.equal s1 s2
+	| (Quote ts1, Quote ts2) -> type_sig_equal ts1 ts2
+	| _ -> false
+
+let rec elem_match input_elem popped_elem = match (input_elem, popped_elem) with
+	| (Poly c, p) -> true
+	| (Mono s1, Mono s2) -> String.equal s1 s2
+	| (Quote ts1, Quote ts2) -> type_sig_match ts1 ts2
+	| _ -> false
+and type_sig_match input_ts popped_ts = match (input_ts, popped_ts) with
+	| (Op (is1, os1), Op (is2, os2)) -> 
+		(List.equal elem_match is1 is2) && (List.equal elem_match os1 os2)
+	| (Apply, Apply) -> true
+	| _ -> false
 
 let push t = Op ([], [t])
 let drop = Op([Poly 'a'], [])
@@ -16,10 +34,17 @@ let quote ts = push (Quote ts)
 let rec type_apply stack ts = match (ts, stack) with	
 	| (Op (inputs, outputs), s) -> (
 		let (popped, remaining) = List.split_n s (List.length inputs) in
-		if (List.equal elem_match popped inputs) then 
-			Ok (outputs @ remaining)
-		else
-			Error "Expected and received are different"
+		let type_map = List.map2_exn inputs popped ~f:(fun i p -> (
+			if elem_match i p then Ok (i, p) else Error "no match"
+		)) |> Result.all in
+		let lookup (e: elem): (elem, string) Result.t =  
+			type_map |> Result.bind ~f:(fun tm -> 
+				List.Assoc.find tm ~equal:elem_equal e
+				|> Result.of_option ~error: "cannot lookup element"
+			) 
+		in
+		let new_outputs = List.map inputs ~f:lookup in
+		new_outputs |> Result.all |> Result.map ~f: (fun nos -> nos @ remaining)
 	)
 	| (Apply, (Quote q_ts)::s) -> type_apply s q_ts
 	| (Apply, _) -> Error "expected quote"
